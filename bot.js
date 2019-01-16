3478672ddefe06d08d506333755b0caa5ac79e7c
hassan-games <45627569+hassan-games@users.noreply.github.com>
2019-01-16 11:15:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,7 +177,7 @@ client.on('guildMemberAdd',async member => {
             let ava = new Avatar;
             ava.src = buf;
             ctx.beginPath();
-            ctx.arc(77, 151, 62, 0, Math.PI*2);
+            ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
             ctx.closePath();
             ctx.clip();
             ctx.drawImage(ava, 13, 38, 128, 126);
