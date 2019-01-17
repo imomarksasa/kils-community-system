e63c48f0f316a6e57da44b53b38b0e93294c965d
hassan-games <45627569+hassan-games@users.noreply.github.com>
2019-01-17 22:56:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -171,7 +171,7 @@ client.on('guildMemberAdd',async member => {
             ctx.fontSize = '72px';
             ctx.fillStyle = "#ffffff";
             ctx.textAlign = "center";
-            ctx.fillText(`${member.guild.memberCount} Members`, 650, 150);
+            ctx.fillText(`${member.guild.memberCount} Members`, 650, 180);
            
             let Avatar = Canvas.Image;
             let ava = new Avatar;
